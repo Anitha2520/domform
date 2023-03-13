ab88231909394a888abae2851ca278c8bab020c2
Anitha2520 <124969957+Anitha2520@users.noreply.github.com>
2023-03-13 12:29:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    var fname=document.getElementById("firstname").value;
+    console.log(fname);
+    var mname=document.getElementById("middlename").value;
+    console.log(mname);
+    var lname=document.getElementById("lastname").value;
+    console.log(lname);
+    var email=document.getElementById("email").value;
+    console.log(email);
+    var password=document.getElementById("password").value;
+    console.log(password);
+}
